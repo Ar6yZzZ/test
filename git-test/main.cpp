2700f737c57c3d7a064cc53bdcdd0f905b8e55db
Roman Shulgan <romashulgan@icloud.com>
2022-12-03 16:39:12 +0300

    new commit

--- git-test/main.cpp
+++ git-test/main.cpp
@@ -1,7 +1,2 @@
 #include <iostream>
-
-int main() {
-    std::string str;
-    std::cin >> str;
-    std::cout << "Hello, World! " << str;
-}
+int main() { std::string str; std::cin >> str; std::cout << "Hello, World! " << str; }
