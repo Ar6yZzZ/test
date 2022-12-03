4a66ee0606244aa94fe1545967d7d473e2dfe716
Roman Shulgan <romashulgan@icloud.com>
2022-12-03 20:48:54 +0300

    Update main.cpp

--- git-test/main.cpp
+++ git-test/main.cpp
@@ -1,2 +1,7 @@
 #include <iostream>
-int main() { std::string str; std::cin >> str; std::cout << "Hello, World! " << str; }
+int main() {
+    std::string str;
+    std::cin >> str;
+    std::cout << "Hello, World! " << str;
+    std::cout << "\nHi my friend!!!";
+}
