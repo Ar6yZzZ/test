3472f2a49ab7dc0f069391d4efac56b4fdbcd6e0
Roman Shulgan <romashulgan@icloud.com>
2022-12-04 23:36:59 +0300

    commit

--- git-test/main.cpp
+++ git-test/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include<iostream>
+
 int main() {
-    int n, l, r; std::cin >> n; int array[n][3], arr[n];
-    for (int i = 1; i <= n; i++) { std::cin >> l >> array[i][1] >> array[i][2]; }
-    for (int i = n; i >= 1; i--) {
-        l = array[i][1] != 0 ? array[array[i][1]][0] + 1 : 0;
-        r = array[i][2] != 0 ? array[array[i][2]][0] + 1 : 0;
-        arr[i] = l - r; array[i][0] = l > r ? l : r;
-    } for (int i = 1; i <= n; i++) std::cout << arr[i] << '\n';
+    int n, a, j = 1; std::cin >> n; n++; int array[n + 1][3];
+    for (int i = 1; i < n; i++) { std::cin >> array[i][0] >> array[i][1] >> array[i][2]; } std::cin >> a;
+    while(true) {
+        if (a > array[j][0]){ if (array[j][2] != 0) j = array[j][2];}
+    }
+    std::cout << j;
 }
